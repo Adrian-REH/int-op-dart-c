76292f2cbd9f427d25219d497d270c07aedbd586
Adrian-REH <adrianherrera.r.e@gmail.com>
2023-07-09 20:24:24 +0200

    first commit

---
+++ bin/int_op_dart_c.dart
@@ -0,0 +1,25 @@
+
+import 'dart:ffi' as ffi;
+import 'dart:io' show Platform, Directory;
+
+import 'package:path/path.dart' as path;
+
+// FFI signature of the hello_world C function
+typedef HelloWorldFunc = ffi.Void Function();
+// Dart type definition for calling the C foreign function
+typedef HelloWorld = void Function();
+
+void main() {
+  // Open the dynamic library
+  var libraryPath  = path.join(Directory.current.path, 'bin','c_generated','out','build','x64-Debug', 'hello.dll');
+
+
+  final dylib = ffi.DynamicLibrary.open(libraryPath);
+
+  // Look up the C function 'hello_world'
+  final HelloWorld hello = dylib
+      .lookup<ffi.NativeFunction<HelloWorldFunc>>('hello_world')
+      .asFunction();
+  // Call the function
+  hello();
+}
---
+++ test/int_op_dart_c_test.dart
@@ -0,0 +1,8 @@
+import 'package:int_op_dart_c/int_op_dart_c.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
